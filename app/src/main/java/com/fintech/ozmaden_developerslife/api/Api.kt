9be57da7ffb9f45f60f625f32606b15ebb0469bf
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-02-04 17:57:22 +0300

    Update Api.kt

--- app/src/main/java/com/fintech/ozmaden_developerslife/api/Api.kt
+++ app/src/main/java/com/fintech/ozmaden_developerslife/api/Api.kt
@@ -1,19 +1,19 @@
 package com.fintech.ozmaden_developerslife.api
 
+import com.fintech.ozmaden_developerslife.model.CategoryPosts
+import com.fintech.ozmaden_developerslife.model.Post
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 internal const val ENDPOINT = "https://developerslife.ru/"
 
 interface Api {
-    //    @GET("/random?json=true")
-    //    fun getRandomPost():
-    //    // Post
+    @GET("/random?json=true")
+    fun getRandomPost(): Post
 
     @GET("/{category}/{page}?json=true")
     fun getCategoryPosts(
         @Path("category") category: String,
         @Path("page") page: Int
-    )
-    // : CategoryPosts
+    ): CategoryPosts
 }
