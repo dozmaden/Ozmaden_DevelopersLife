ba11b1bbda9880ee1f3ba6c822ebe6ef00b1deb3
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-02-04 23:35:42 +0300

    Update TopPostViewModel.kt

--- app/src/main/java/com/fintech/ozmaden_developerslife/ui/top/TopPostViewModel.kt
+++ app/src/main/java/com/fintech/ozmaden_developerslife/ui/top/TopPostViewModel.kt
@@ -1,22 +1,9 @@
 package com.fintech.ozmaden_developerslife.ui.top
 
-import androidx.lifecycle.viewModelScope
-import com.fintech.ozmaden_developerslife.ui.PostViewModel
-import kotlinx.coroutines.launch
+import com.fintech.ozmaden_developerslife.ui.CategoryPostViewModel
 
-class TopPostViewModel : PostViewModel() {
-    private var page: Int = 0
-
-    override suspend fun loadNewPost() {
-        val category = "top"
-        viewModelScope.launch {
-            val newPosts = postRepository.getCategoryPosts(category, page)
-            newPosts?.let {
-                position++
-                page++
-                postHistory.addAll(newPosts)
-                _post.postValue(postHistory.elementAt(position))
-            }
-        }
+class TopPostViewModel : CategoryPostViewModel() {
+    override fun category(): String {
+        return "top"
     }
 }
