7f735e14fee8d2287af1d253d446e38a54da6181
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-02-04 22:24:07 +0300

    Refactoring

--- app/src/main/java/com/fintech/ozmaden_developerslife/ui/random/RandomFragment.kt
+++ app/src/main/java/com/fintech/ozmaden_developerslife/ui/random/RandomFragment.kt
@@ -1,7 +1,6 @@
 package com.fintech.ozmaden_developerslife.ui.random
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
