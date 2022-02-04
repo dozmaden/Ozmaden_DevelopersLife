30ddb454475c86326448ba26b34f2a604a29c22d
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-02-04 22:16:12 +0300

    Add OkHttp logging
    - for debug reasons as of now

--- app/src/main/java/com/fintech/ozmaden_developerslife/api/RetrofitInstance.kt
+++ app/src/main/java/com/fintech/ozmaden_developerslife/api/RetrofitInstance.kt
@@ -1,12 +1,23 @@
 package com.fintech.ozmaden_developerslife.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
+    private val logInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val httpClient = OkHttpClient.Builder()
+        .addNetworkInterceptor(logInterceptor)
+        .build()
+
     private val retrofit = Retrofit.Builder()
-        .baseUrl(ENDPOINT)
+        .client(httpClient)
         .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(ENDPOINT)
         .build()
 
     val api: Api = retrofit.create(Api::class.java)
