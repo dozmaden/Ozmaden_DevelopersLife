88afa74ba79076fdcb3e9a491f0dcdd89e9e00da
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-02-04 20:08:17 +0300

    Made Random Page default
    Now the application starts with Random Page, instead of Home Page

---
+++ app/src/main/java/com/fintech/ozmaden_developerslife/ui/random/RandomFragment.kt
@@ -0,0 +1,55 @@
+package com.fintech.ozmaden_developerslife.ui.random
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.fintech.ozmaden_developerslife.databinding.FragmentRandomBinding
+
+class RandomFragment : Fragment() {
+
+    private lateinit var viewModel: RandomViewModel
+    private var _binding: FragmentRandomBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        viewModel =
+            ViewModelProvider(this).get(RandomViewModel::class.java)
+
+        _binding = FragmentRandomBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textRandom
+
+        viewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+
+        val gifView: ImageView = binding.gif
+
+        viewModel.post.observe(viewLifecycleOwner, Observer {
+            Glide.with(this).load(it.gifURL.replace("http", "https")).into(gifView);
+        })
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/fintech/ozmaden_developerslife/ui/random/RandomViewModel.kt
+++ app/src/main/java/com/fintech/ozmaden_developerslife/ui/random/RandomViewModel.kt
@@ -1,4 +1,51 @@
 package com.fintech.ozmaden_developerslife.ui.random
 
-class RandomViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fintech.ozmaden_developerslife.model.Post
+import com.fintech.ozmaden_developerslife.repository.PostRepository
+import kotlinx.coroutines.launch
+
+class RandomViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is Random Fragment"
+    }
+
+    val text: LiveData<String> = _text
+
+    //
+    private val _post = MutableLiveData<Post>()
+    val post: LiveData<Post> = _post
+
+    protected val postRepository = PostRepository()
+
+    init {
+        loadPost()
+    }
+
+    private fun loadPost() {
+        viewModelScope.launch {
+            try {
+                loadNewPost()
+            } catch (e: Exception) {
+                Log.w("RandomViewModel", e.message.toString())
+            }
+        }
+    }
+
+    private suspend fun loadNewPost() {
+        viewModelScope.launch {
+            val rndPost = postRepository.getRandomPost()
+            if (rndPost != null) {
+                _post.postValue(rndPost!!)
+                Log.d("RandomViewModel", "Got random post and inserted it!")
+            } else {
+                Log.d("RandomViewModel", "Could not insert post!")
+            }
+        }
+    }
 }
